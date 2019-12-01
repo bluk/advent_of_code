ec10630bedfee696c2c6a9c14a0d64b1ab1adbe8
Bryant Luk <bryant.luk@bryantluk.com>
2019-12-01 17:41:14 -0600

    Finish Day 1 Part Two

--- aoc_2019_1/src/lib.rs
+++ aoc_2019_1/src/lib.rs
@@ -6,7 +6,18 @@
 /// assert_eq!(2, find_fuel_required(12));
 /// ```
 pub fn find_fuel_required(mass: u32) -> u32 {
-    (mass / 3) - 2
+    let mut result = mass / 3;
+    if result < 2 {
+        0
+    } else {
+        result -= 2;
+
+        if result == 0 {
+            result
+        } else {
+            result + find_fuel_required(result)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -28,12 +39,12 @@ mod test {
     #[test]
     fn ex3() {
         let mass = 1969;
-        assert_eq!(654, find_fuel_required(mass));
+        assert_eq!(966, find_fuel_required(mass));
     }
 
     #[test]
     fn ex4() {
         let mass = 100_756;
-        assert_eq!(33_583, find_fuel_required(mass));
+        assert_eq!(50346, find_fuel_required(mass));
     }
 }
