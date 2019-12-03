ab8066fb88e60918a300bffbbc8796402000ccf8
Bryant Luk <bryant.luk@bryantluk.com>
2019-12-03 11:43:30 -0600

    Finish 2019 Day Three Part Two

--- aoc_2019_3/src/lib.rs
+++ aoc_2019_3/src/lib.rs
@@ -1,11 +1,14 @@
 use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::iter::FromIterator;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
 pub enum Move {
-    Up(isize),
-    Down(isize),
-    Left(isize),
-    Right(isize),
+    Up(usize),
+    Down(usize),
+    Left(usize),
+    Right(usize),
 }
 
 pub fn parse_moves(input: &str) -> Result<Vec<Move>, std::num::ParseIntError> {
@@ -15,7 +18,7 @@ pub fn parse_moves(input: &str) -> Result<Vec<Move>, std::num::ParseIntError> {
         .map(|s| {
             let s = s.trim();
             let dir = &s[0..1];
-            let value = s[1..].parse::<isize>()?;
+            let value = s[1..].parse::<usize>()?;
             Ok(match dir {
                 "U" => Move::Up(value),
                 "D" => Move::Down(value),
@@ -27,22 +30,23 @@ pub fn parse_moves(input: &str) -> Result<Vec<Move>, std::num::ParseIntError> {
         .collect::<Result<Vec<Move>, std::num::ParseIntError>>()
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 pub struct Pos {
     x: isize,
     y: isize,
 }
 
 impl Pos {
-    pub fn manhattan_dist(&self) -> isize {
-        self.x.abs() + self.y.abs()
+    pub fn manhattan_dist(&self) -> usize {
+        usize::try_from(self.x.abs()).unwrap() + usize::try_from(self.y.abs()).unwrap()
     }
 }
 
-pub fn visited_pos(moves: &[Move]) -> BTreeSet<Pos> {
-    let mut pos = BTreeSet::new();
+pub fn visited_pos_with_steps(moves: &[Move]) -> HashMap<Pos, usize> {
+    let mut pos = HashMap::new();
     let mut x = 0;
     let mut y = 0;
+    let mut step = 0;
 
     for m in moves {
         let (dx, dy, len) = match m {
@@ -55,18 +59,60 @@ pub fn visited_pos(moves: &[Move]) -> BTreeSet<Pos> {
         (0..*len).for_each(|_| {
             x += dx;
             y += dy;
-            pos.insert(Pos { x, y });
+            step += 1;
+
+            let p = Pos { x, y };
+            pos.entry(p).or_insert(step);
         })
     }
 
     pos
 }
 
+pub fn find_closest_manhattan_dist(
+    input1: &str,
+    input2: &str,
+) -> Result<Option<usize>, std::num::ParseIntError> {
+    let moves1 = parse_moves(&input1)?;
+    let moves2 = parse_moves(&input2)?;
+    let visited_pos_with_steps1 = visited_pos_with_steps(&moves1);
+    let visited_pos_with_steps2 = visited_pos_with_steps(&moves2);
+    let visited_pos1 = BTreeSet::from_iter(visited_pos_with_steps1.keys());
+    let visited_pos2 = BTreeSet::from_iter(visited_pos_with_steps2.keys());
+
+    let intersection: BTreeSet<_> = visited_pos1.intersection(&visited_pos2).cloned().collect();
+    Ok(intersection.into_iter().map(|&p| p.manhattan_dist()).min())
+}
+
+pub fn find_fewest_steps(
+    input1: &str,
+    input2: &str,
+) -> Result<Option<usize>, std::num::ParseIntError> {
+    let moves1 = parse_moves(&input1)?;
+    let moves2 = parse_moves(&input2)?;
+    let visited_pos_with_steps1 = visited_pos_with_steps(&moves1);
+    let visited_pos_with_steps2 = visited_pos_with_steps(&moves2);
+    let visited_pos1 = BTreeSet::from_iter(visited_pos_with_steps1.keys());
+    let visited_pos2 = BTreeSet::from_iter(visited_pos_with_steps2.keys());
+
+    let intersection: BTreeSet<_> = visited_pos1.intersection(&visited_pos2).cloned().collect();
+    Ok(intersection
+        .into_iter()
+        .filter_map(|p| {
+            match (
+                visited_pos_with_steps1.get(p),
+                visited_pos_with_steps2.get(p),
+            ) {
+                (Some(dist1), Some(dist2)) => Some(dist1 + dist2),
+                _ => None,
+            }
+        })
+        .min())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-
-    use std::iter::FromIterator;
 
     #[test]
     fn parse() {
@@ -88,15 +134,15 @@ mod test {
         let moves = parse_moves(&input).unwrap();
         assert_eq!(
             BTreeSet::from_iter(vec![
-                Pos { x: 0, y: 1 },
-                Pos { x: 0, y: 2 },
-                Pos { x: 0, y: 3 },
-                Pos { x: -1, y: 1 },
-                Pos { x: -2, y: 1 },
-                Pos { x: -3, y: 1 },
-                Pos { x: -4, y: 1 },
+                &Pos { x: 0, y: 1 },
+                &Pos { x: 0, y: 2 },
+                &Pos { x: 0, y: 3 },
+                &Pos { x: -1, y: 1 },
+                &Pos { x: -2, y: 1 },
+                &Pos { x: -3, y: 1 },
+                &Pos { x: -4, y: 1 },
             ]),
-            visited_pos(&moves)
+            BTreeSet::from_iter(visited_pos_with_steps(&moves).keys())
         );
     }
 
@@ -104,60 +150,41 @@ mod test {
     fn ex1() {
         let input1 = "R8,U5,L5,D3";
         let input2 = "U7,R6,D4,L4";
-        let moves1 = parse_moves(&input1).unwrap();
-        let moves2 = parse_moves(&input2).unwrap();
-        let pos1 = visited_pos(&moves1);
-        let pos2 = visited_pos(&moves2);
-
-        let intersection: BTreeSet<_> = pos1.intersection(&pos2).cloned().collect();
-        println!("{:?}", intersection);
-        assert_eq!(
-            intersection
-                .iter()
-                .map(|p| p.manhattan_dist())
-                .min()
-                .unwrap(),
-            6
-        );
+        assert_eq!(find_closest_manhattan_dist(&input1, &input2), Ok(Some(6)));
     }
 
     #[test]
     fn ex2() {
         let input1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
         let input2 = "U62,R66,U55,R34,D71,R55,D58,R83";
-        let moves1 = parse_moves(&input1).unwrap();
-        let moves2 = parse_moves(&input2).unwrap();
-        let pos1 = visited_pos(&moves1);
-        let pos2 = visited_pos(&moves2);
-
-        let intersection: BTreeSet<_> = pos1.intersection(&pos2).cloned().collect();
-        assert_eq!(
-            intersection
-                .iter()
-                .map(|p| p.manhattan_dist())
-                .min()
-                .unwrap(),
-            159
-        );
+        assert_eq!(find_closest_manhattan_dist(&input1, &input2), Ok(Some(159)));
     }
 
     #[test]
     fn ex3() {
         let input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
         let input2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
-        let moves1 = parse_moves(&input1).unwrap();
-        let moves2 = parse_moves(&input2).unwrap();
-        let pos1 = visited_pos(&moves1);
-        let pos2 = visited_pos(&moves2);
+        assert_eq!(find_closest_manhattan_dist(&input1, &input2), Ok(Some(135)));
+    }
 
-        let intersection: BTreeSet<_> = pos1.intersection(&pos2).cloned().collect();
-        assert_eq!(
-            intersection
-                .iter()
-                .map(|p| p.manhattan_dist())
-                .min()
-                .unwrap(),
-            135
-        );
+    #[test]
+    fn ex4() {
+        let input1 = "R8,U5,L5,D3";
+        let input2 = "U7,R6,D4,L4";
+        assert_eq!(find_fewest_steps(&input1, &input2), Ok(Some(30)));
+    }
+
+    #[test]
+    fn ex5() {
+        let input1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+        let input2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+        assert_eq!(find_fewest_steps(&input1, &input2), Ok(Some(610)));
+    }
+
+    #[test]
+    fn ex6() {
+        let input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+        let input2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+        assert_eq!(find_fewest_steps(&input1, &input2), Ok(Some(410)));
     }
 }
--- aoc_2019_3/src/main.rs
+++ aoc_2019_3/src/main.rs
@@ -1,51 +1,38 @@
-use std::collections::BTreeSet;
 use std::io::{self};
 
 use aoc_2019_3;
 
 fn main() {
-    let mut input = String::new();
-    let moves1 = match io::stdin().read_line(&mut input) {
-        Ok(_) => match aoc_2019_3::parse_moves(&input) {
-            Ok(moves) => moves,
-            Err(e) => {
-                eprintln!("error: {}", e);
-                std::process::exit(1);
-            }
-        },
+    let mut input1 = String::new();
+    match io::stdin().read_line(&mut input1) {
+        Ok(_) => {}
         Err(e) => {
             eprintln!("error: {}", e);
             std::process::exit(1);
         }
     };
 
-    let mut input = String::new();
-    let moves2 = match io::stdin().read_line(&mut input) {
-        Ok(_) => match aoc_2019_3::parse_moves(&input) {
-            Ok(moves) => moves,
-            Err(e) => {
-                eprintln!("error: {}", e);
-                std::process::exit(1);
-            }
-        },
+    let mut input2 = String::new();
+    match io::stdin().read_line(&mut input2) {
+        Ok(_) => {}
         Err(e) => {
             eprintln!("error: {}", e);
             std::process::exit(1);
         }
     };
 
-    let visited_pos1 = aoc_2019_3::visited_pos(&moves1);
-    let visited_pos2 = aoc_2019_3::visited_pos(&moves2);
-    let intersection: BTreeSet<_> = visited_pos1.intersection(&visited_pos2).cloned().collect();
-    let closest_dist = intersection.iter().map(|p| p.manhattan_dist()).min();
-
-    if let Some(closest_dist) = closest_dist {
-        println!("{}", closest_dist);
-    } else {
-        eprintln!(
-            "could not find intersection between {:?} and {:?}",
-            visited_pos1, visited_pos2
-        );
-        std::process::exit(1);
+    match aoc_2019_3::find_fewest_steps(&input1, &input2) {
+        Ok(fewest_steps) => {
+            if let Some(fewest_steps) = fewest_steps {
+                println!("{}", fewest_steps);
+            } else {
+                eprintln!("could not find the fewest steps");
+                std::process::exit(1);
+            }
+        }
+        Err(e) => {
+            eprintln!("error: {}", e);
+            std::process::exit(1);
+        }
     }
 }
