25835abb508753073989a343d40dba3a30874105
Bryant Luk <bryant.luk@bryantluk.com>
2019-12-13 11:32:14 -0600

    Finish 2019 Day 13 Part Two

--- aoc_2019_13/src/arcade.rs
+++ aoc_2019_13/src/arcade.rs
@@ -1,8 +1,9 @@
+use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::str::FromStr;
 
 use crate::error::Error;
-use crate::intcode::Prog;
+use crate::intcode::{Prog, ProgState};
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 pub enum Type {
@@ -29,35 +30,147 @@ impl FromStr for Type {
 }
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Pos {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 pub struct Tile {
-    pub x: i64,
-    pub y: i64,
+    pub pos: Pos,
     pub tile_id: Type,
 }
 
-pub fn run_loop(mut prog: Prog) -> Result<Vec<Tile>, Error> {
-    let mut tiles = Vec::<Tile>::new();
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub enum Joystick {
+    Neutral,
+    Left,
+    Right,
+}
 
+impl From<Joystick> for String {
+    fn from(other: Joystick) -> String {
+        match other {
+            Joystick::Neutral => String::from("0"),
+            Joystick::Left => String::from("-1"),
+            Joystick::Right => String::from("1"),
+        }
+    }
+}
+
+pub fn run_loop(mut prog: Prog) -> Result<i64, Error> {
     let mut input = VecDeque::new();
     let mut output = VecDeque::new();
 
-    prog.run(&mut input, &mut output)?;
+    let mut score: i64 = 0;
+    let mut tiles = Vec::<Tile>::new();
 
-    while let Some(x) = output.pop_front() {
-        if let Some(y) = output.pop_front() {
-            if let Some(id) = output.pop_front() {
-                tiles.push(Tile {
-                    x: x.parse::<i64>()?,
-                    y: y.parse::<i64>()?,
-                    tile_id: Type::from_str(&id)?,
-                })
+    loop {
+        let mut new_tiles = Vec::<Tile>::new();
+        prog.run(&mut input, &mut output)?;
+
+        while let Some(x) = output.pop_front() {
+            if let Some(y) = output.pop_front() {
+                if let Some(id) = output.pop_front() {
+                    if x == "-1" && y == "0" {
+                        score = id.parse::<i64>()?;
+                    } else {
+                        new_tiles.push(Tile {
+                            pos: Pos {
+                                x: x.parse::<i64>()?,
+                                y: y.parse::<i64>()?,
+                            },
+                            tile_id: Type::from_str(&id)?,
+                        })
+                    }
+                } else {
+                    panic!("unexpected program state");
+                }
             } else {
                 panic!("unexpected program state");
             }
-        } else {
-            panic!("unexpected program state");
+        }
+
+        let pos_to_replace = new_tiles.iter().map(|t| t.pos).collect::<Vec<Pos>>();
+
+        tiles = tiles
+            .into_iter()
+            .filter(|t| !pos_to_replace.contains(&t.pos))
+            .collect::<Vec<Tile>>();
+
+        tiles.append(&mut new_tiles);
+
+        // display(tiles.clone());
+
+        match prog.state() {
+            ProgState::Halt => break,
+            ProgState::NotStarted => unreachable!(),
+            ProgState::NeedInput => {
+                let paddle = tiles
+                    .iter()
+                    .find(|&&t| t.tile_id == Type::HorizontalPaddle)
+                    .expect("paddle to exist");
+
+                let ball = tiles
+                    .iter()
+                    .find(|&&t| t.tile_id == Type::Ball)
+                    .expect("ball to exist");
+
+                if ball.pos.x == paddle.pos.x {
+                    input.push_back(String::from(Joystick::Neutral));
+                } else if ball.pos.x > paddle.pos.x {
+                    input.push_back(String::from(Joystick::Right));
+                } else if ball.pos.x < paddle.pos.x {
+                    input.push_back(String::from(Joystick::Left));
+                }
+            }
         }
     }
 
-    Ok(tiles)
+    Ok(score)
+}
+
+pub fn display(mut tiles: Vec<Tile>) {
+    let max_x = tiles.iter().map(|t| t.pos.x).max().unwrap();
+    let min_x = tiles.iter().map(|t| t.pos.x).min().unwrap();
+    let max_y = tiles.iter().map(|t| t.pos.y).max().unwrap();
+    let min_y = tiles.iter().map(|t| t.pos.y).min().unwrap();
+
+    tiles.sort_by(|a, b| {
+        if a.pos.y > b.pos.y {
+            Ordering::Less
+        } else if a.pos.y < b.pos.y {
+            Ordering::Greater
+        } else {
+            a.pos.x.cmp(&b.pos.x)
+        }
+    });
+
+    let mut iter = tiles.iter().peekable();
+
+    let rng = max_y - min_y;
+    for y in 0..=rng {
+        let y = max_y - y;
+        for x in min_x..=max_x {
+            if let Some(&next_tile) = iter.peek() {
+                let pos = Pos { x, y };
+                if next_tile.pos == pos {
+                    print!(
+                        "{}",
+                        match next_tile.tile_id {
+                            Type::Empty => " ",
+                            Type::Wall => "|",
+                            Type::Block => "=",
+                            Type::HorizontalPaddle => "-",
+                            Type::Ball => "*",
+                        }
+                    );
+                    iter.next();
+                }
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
 }
--- aoc_2019_13/src/main.rs
+++ aoc_2019_13/src/main.rs
@@ -1,28 +1,17 @@
 use std::io::{self};
 
-use aoc_2019_13::{
-    self,
-    arcade::{Tile, Type},
-    error::Error,
-    intcode::Prog,
-};
+use aoc_2019_13::{self, error::Error, intcode::Prog};
 
 fn main() -> Result<(), Error> {
     let mut input = String::new();
     let _ = io::stdin().read_line(&mut input)?;
-    let mem_state = aoc_2019_13::intcode::parse_mem_state(&input)?;
+    let mut mem_state = aoc_2019_13::intcode::parse_mem_state(&input)?;
+    mem_state[0] = 2;
 
     let prog = Prog::new(&mem_state);
-    let tiles = aoc_2019_13::arcade::run_loop(prog)?;
+    let score = aoc_2019_13::arcade::run_loop(prog)?;
 
-    println!(
-        "{}",
-        tiles
-            .into_iter()
-            .filter(|t| t.tile_id == Type::Block)
-            .collect::<Vec<Tile>>()
-            .len()
-    );
+    println!("{}", score);
 
     Ok(())
 }
