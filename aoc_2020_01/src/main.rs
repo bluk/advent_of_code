d4d78663cca0aaf1280804f8c917117a0f0c7c27
Bryant Luk <bryant.luk@bryantluk.com>
2020-12-01 17:05:07 -0600

    Finish 2020 Day 1 Part Two

--- aoc_2020_01/src/main.rs
+++ aoc_2020_01/src/main.rs
@@ -3,37 +3,86 @@ use std::io::{self, BufRead};
 #[derive(Debug)]
 enum Error {
     ParseError,
-    InvalidInput,
+    InvalidArg,
 }
 
-fn find_sum_2020(input: &[u32]) -> Result<(u32, u32), Error> {
-    let mut remaining_data = input;
-    loop {
-        if let Some((first, remainder)) = remaining_data.split_first() {
-            remaining_data = remainder;
+fn find_combination_sum<T>(input: &[T], k: usize, target_sum: T) -> Result<Option<Vec<T>>, Error>
+where
+    T: std::iter::Sum,
+    T: std::cmp::PartialEq,
+    T: Copy,
+{
+    if k == 0 {
+        return Err(Error::InvalidArg);
+    }
 
-            for r in remaining_data {
-                if first + r == 2020 {
-                    return Ok((*first, *r));
+    if input.len() < k {
+        return Err(Error::InvalidArg);
+    }
+
+    let mut indices: Vec<usize> = (0..k).collect();
+
+    loop {
+        // println!("{:?}", &indices);
+        let sum: T = indices.iter().map(|i| input[*i]).sum();
+        if sum == target_sum {
+            return Ok(Some(indices.iter().map(|i| input[*i]).collect()));
+        }
+
+        // From the indices right to left, increment the rightmost index value which is not already
+        // at its maximum value.
+        //
+        // Assuming 4 input elements.
+        //
+        // If the indexes are: [0, 1, 2], then the next iteration should be [0, 1, 3].
+        //
+        // If the indexes are: [0, 1, 3], then the next iteration should be [0, 2, 3].
+        //
+        // If the indexes are: [0, 2, 3], then the next iteration should be [1, 2, 3].
+        let mut indices_index = indices.len() - 1;
+        loop {
+            if indices[indices_index] < input.len() - 1 - (indices.len() - 1 - indices_index) {
+                indices[indices_index] += 1;
+
+                // Reset all the indexes to the "right" to a value greater than any on the "left".
+                let mut val = indices[indices_index];
+                loop {
+                    indices_index += 1;
+                    if indices_index >= indices.len() {
+                        break;
+                    }
+                    val += 1;
+                    indices[indices_index] = val;
                 }
+                break;
+            } else {
+                if indices_index == 0 {
+                    return Ok(None);
+                }
+
+                indices_index -= 1;
             }
-        } else {
-            return Err(Error::InvalidInput);
         }
     }
 }
 
 fn main() -> Result<(), Error> {
-    let stdin = io::stdin();
-    let mut input: Vec<u32> = Vec::new();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        input.push(line.parse().map_err(|_| Error::ParseError)?)
-    }
+    let input = io::stdin()
+        .lock()
+        .lines()
+        .map(|l| l.unwrap().parse())
+        .collect::<Result<Vec<u32>, _>>()
+        .map_err(|_| Error::ParseError)?;
 
-    if let Ok((first, second)) = find_sum_2020(&input) {
-        println!("First Part Answer: {}", first * second);
-    }
+    let values = find_combination_sum(&input, 2, 2020)
+        .expect("No invalid arguments")
+        .expect("An answer to exist");
+    println!("First Part Answer: {}", values.iter().product::<u32>());
+
+    let values = find_combination_sum(&input, 3, 2020)
+        .expect("No invalid arguments")
+        .expect("An answer to exist");
+    println!("Second Part Answer: {}", values.iter().product::<u32>());
 
     Ok(())
 }
