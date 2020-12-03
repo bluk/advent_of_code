a867664b9efd4e168b6495d2a30900b5444c8d21
Bryant Luk <bryant.luk@bryantluk.com>
2020-12-03 13:43:07 -0600

    Finish 2020 Day 2 Part Two

--- aoc_2020_02/src/main.rs
+++ aoc_2020_02/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         match (space_split.next(), space_split.next(), space_split.next()) {
             (Some(count), Some(ch), Some(password)) => {
                 let mut split_count = count.split("-");
-                let (min_range, max_range): (usize, usize) =
+                let (first_pos, second_pos): (usize, usize) =
                     match (split_count.next(), split_count.next()) {
                         (Some(min), Some(max)) => (
                             min.parse().expect("a number"),
@@ -20,10 +20,21 @@ fn main() {
                     };
                 let ch = ch.chars().next().unwrap();
 
-                let count = password.chars().filter(|c| *c == ch).count();
-                if (min_range..=max_range).contains(&count) {
-                    valid_count += 1;
-                }
+                let password = password.chars();
+                let mut password = password.skip(first_pos - 1);
+                let first = password.next();
+                let mut password = password.skip(second_pos - first_pos - 1);
+                let second = password.next();
+                match (first, second) {
+                    (Some(first), Some(second))
+                        if (first == ch && second != ch) || (first != ch && second == ch) =>
+                    {
+                        valid_count += 1;
+                    }
+                    (Some(first), None) if first == ch => valid_count += 1,
+                    (None, Some(second)) if second == ch => valid_count += 1,
+                    _ => {}
+                };
             }
             _ => panic!("Unexpected input"),
         }
