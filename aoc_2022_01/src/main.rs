c661a265ef1ec773799a8b2fa0d6d991c2e6f60b
Bryant Luk <code@bryantluk.com>
2022-12-01 10:19:38 -0600

    Finish 2022 Day 1 Part Two

--- aoc_2022_01/src/main.rs
+++ aoc_2022_01/src/main.rs
@@ -1,27 +1,32 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let mut max_calories = 0;
+    const TOP_ELVES_COUNT: usize = 3;
+
+    let mut top_calories = [0; TOP_ELVES_COUNT];
 
     let mut calories = 0;
 
     for line in io::stdin().lines() {
         let line = line?;
         if line.is_empty() {
-            if max_calories < calories {
-                max_calories = calories;
+            let smallest_top_calories = top_calories
+                .first_mut()
+                .expect("top_calories should have at least one element");
+            if *smallest_top_calories < calories {
+                *smallest_top_calories = calories;
+                top_calories.sort_unstable();
             }
             calories = 0;
-            continue;
+        } else {
+            let num = line
+                .parse::<i32>()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            calories += num;
         }
-
-        let num = line
-            .parse::<i32>()
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        calories += num;
     }
 
-    println!("{max_calories}");
+    println!("{}", top_calories.iter().sum::<i32>());
 
     Ok(())
 }
