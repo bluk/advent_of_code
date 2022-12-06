5c30e61bd18f6b3b37658bab927902a09f224aca
Bryant Luk <code@bryantluk.com>
2022-12-06 09:06:36 -0600

    Finish 2022 Day 6 Part Two

--- aoc_2022_06/src/main.rs
+++ aoc_2022_06/src/main.rs
@@ -4,8 +4,10 @@ use std::{
     io::{self, Read},
 };
 
+const START_MSG_DISTINCT_CHARS_COUNT: usize = 14;
+
 fn main() -> io::Result<()> {
-    let mut offset = 4;
+    let mut offset = START_MSG_DISTINCT_CHARS_COUNT;
 
     let mut stream: VecDeque<u8> = VecDeque::default();
 
@@ -20,16 +22,20 @@ fn main() -> io::Result<()> {
 
         stream.extend(&buf[..read]);
 
-        for (a, b, c, d) in stream.iter().tuple_windows() {
-            let set = [a, b, c, d];
-            if set.iter().unique().count() == set.len() {
+        while stream.len() >= START_MSG_DISTINCT_CHARS_COUNT {
+            if stream
+                .iter()
+                .take(START_MSG_DISTINCT_CHARS_COUNT)
+                .unique()
+                .count()
+                == START_MSG_DISTINCT_CHARS_COUNT
+            {
                 break;
             }
-            offset += 1;
-        }
 
-        if stream.len() > 4 {
-            stream.drain(..stream.len() - 3);
+            stream.pop_front();
+
+            offset += 1;
         }
     }
 
