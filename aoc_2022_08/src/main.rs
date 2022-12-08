ba65b7ea0acfc151874bbf0c187eac9cf69ac203
Bryant Luk <code@bryantluk.com>
2022-12-08 10:35:01 -0600

    Finish 2022 Day 8 Part Two

--- aoc_2022_08/src/main.rs
+++ aoc_2022_08/src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use itertools::{FoldWhile, Itertools as _};
+use std::{cmp::Ordering, io};
 
 fn is_visible_from_edge(x: usize, y: usize, grid: &[Vec<u32>]) -> bool {
     let row = &grid[y];
@@ -24,6 +25,61 @@ fn is_visible_from_edge(x: usize, y: usize, grid: &[Vec<u32>]) -> bool {
     true
 }
 
+fn scenic_score(x: usize, y: usize, grid: &[Vec<u32>]) -> u32 {
+    let row = &grid[y];
+    let min_height = row[x];
+
+    // Left
+    let left_score = row
+        .iter()
+        .rev()
+        .skip(row.len() - x)
+        .fold_while(0, |acc, h| match h.cmp(&min_height) {
+            Ordering::Greater | Ordering::Equal => FoldWhile::Done(acc + 1),
+            Ordering::Less => FoldWhile::Continue(acc + 1),
+        })
+        .into_inner();
+
+    // Right
+    let right_score = row
+        .iter()
+        .skip(x + 1)
+        .fold_while(0, |acc, h| match h.cmp(&min_height) {
+            Ordering::Greater | Ordering::Equal => FoldWhile::Done(acc + 1),
+            Ordering::Less => FoldWhile::Continue(acc + 1),
+        })
+        .into_inner();
+
+    // Up
+    let up_score = grid
+        .iter()
+        .rev()
+        .skip(grid.len() - y)
+        .fold_while(0, |acc, row| {
+            let h = row[x];
+            match h.cmp(&min_height) {
+                Ordering::Greater | Ordering::Equal => FoldWhile::Done(acc + 1),
+                Ordering::Less => FoldWhile::Continue(acc + 1),
+            }
+        })
+        .into_inner();
+
+    // Down
+    let down_score = grid
+        .iter()
+        .skip(y + 1)
+        .fold_while(0, |acc, row| {
+            let h = row[x];
+            match h.cmp(&min_height) {
+                Ordering::Greater | Ordering::Equal => FoldWhile::Done(acc + 1),
+                Ordering::Less => FoldWhile::Continue(acc + 1),
+            }
+        })
+        .into_inner();
+
+    left_score * right_score * up_score * down_score
+}
+
 fn main() -> io::Result<()> {
     let grid = itertools::process_results(io::stdin().lines(), |it| {
         it.map(|line| {
@@ -41,11 +97,10 @@ fn main() -> io::Result<()> {
 
     assert!(grid.iter().all(|row| row.len() == row_len));
 
-    let answer = (0..grid.len()).fold(0, |acc, y| {
-        acc + (0..row_len).fold(0, |acc, x| {
-            acc + u32::from(!is_visible_from_edge(y, x, &grid))
-        })
-    });
+    let answer = (0..grid.len())
+        .filter_map(|y| (0..row_len).map(|x| scenic_score(x, y, &grid)).max())
+        .max()
+        .expect("grid is empty");
 
     println!("{answer}");
 
